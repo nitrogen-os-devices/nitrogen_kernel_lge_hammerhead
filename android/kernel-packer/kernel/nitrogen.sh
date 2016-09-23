78552e862874364bf9a13fef4a9f3565006050a8
Mr-MEX <binder_z@mail.ru>
2016-09-24 02:36:50 +0400

    kernel packer: init.d: fixes and cleanup

--- android/kernel-packer/kernel/nitrogen.sh
+++ android/kernel-packer/kernel/nitrogen.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 
-mv /system/bin/thermal-engine-hh-bak /system/bin/thermal-engine-hh
-mv /system/bin/mpdecision-bak /system/bin/mpdecision
+mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh-bak
+mv /system/bin/mpdecision /system/bin/mpdecision-bak
 
 fstrim -v /cache
 fstrim -v /data
