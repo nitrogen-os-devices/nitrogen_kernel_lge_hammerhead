d538160a34bb3619a3d85340470a3b4123a24751
Mr-MEX <binder_z@mail.ru>
2016-09-30 14:19:33 +0400

    Fix missmerge

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -40,25 +40,4 @@ struct msm_thermal_data {
 #endif
 };
 
-#if defined(CONFIG_INTELLI_THERMAL_V2)
-extern int msm_thermal_init(struct msm_thermal_data *pdata);
-extern int msm_thermal_device_init(void);
-extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
-	bool is_max);
-#else
-static inline int msm_thermal_init(struct msm_thermal_data *pdata)
-{
-	return -ENOSYS;
-}
-static inline int msm_thermal_device_init(void)
-{
-	return -ENOSYS;
-}
-static inline int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
-	bool is_max)
-{
-	return -ENOSYS;
-}
-#endif
-
 #endif /*__MSM_THERMAL_H*/
